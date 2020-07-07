e0a1d7b500aa88372d47a0fbf723890dfc195fc6
Julie Herrick <julieherrick@fb.com>
2020-07-07 13:17:17 -0500

    parse set up

--- Instagram/AppDelegate.m
+++ Instagram/AppDelegate.m
@@ -20,24 +20,11 @@
     // Override point for customization after application launch.
     ParseClientConfiguration *config = [ParseClientConfiguration   configurationWithBlock:^(id<ParseMutableClientConfiguration> configuration) {
         
-        configuration.applicationId = @"codepathInstagram";
-        configuration.server = @"http://codepathfbinstagram.herokuapp.com/parse";
+        configuration.applicationId = @"julieInsta";
+        configuration.server = @"https://julie-insta.herokuapp.com/parse";
     }];
     
     [Parse initializeWithConfiguration:config];
-    
-    
-    PFObject *gameScore = [PFObject objectWithClassName:@"GameScore"];
-    gameScore[@"score"] = @1337;
-    gameScore[@"playerName"] = @"Sean Plott";
-    gameScore[@"cheatMode"] = @NO;
-    [gameScore saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
-      if (succeeded) {
-         NSLog(@"Object saved!");
-      } else {
-         NSLog(@"Error: %@", error.description);
-      }
-    }];
     
     return YES;
 }
